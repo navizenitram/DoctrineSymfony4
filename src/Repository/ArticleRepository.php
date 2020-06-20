0895b6c00104d04a250f9e498067ecd88788b50f
Ivan Martinez <ivan@degustabox.com>
2020-06-20 18:06:29 +0200

    Doctrine Symfony

--- src/Repository/ArticleRepository.php
+++ src/Repository/ArticleRepository.php
@@ -47,4 +47,18 @@ class ArticleRepository extends ServiceEntityRepository
         ;
     }
     */
+    /**
+     * @return Article[]
+     */
+    public function findAllPublishedOrderedByNewest()
+    {
+        return $this->createQueryBuilder('a')
+                    ->andWhere('a.exampleField = :val')
+                    ->setParameter('val', $value)
+                    ->orderBy('a.id', 'ASC')
+                    ->setMaxResults(10)
+                    ->getQuery()
+                    ->getResult()
+            ;
+    }
 }
